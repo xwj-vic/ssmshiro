3b253d87b260be74e11091924088ed85656ea367
541535798@qq.com <541535798@qq.com>
2017-03-08 22:16:07 +0800

    加入kaptcha验证码框架并实现验证码

--- src/main/java/web/LoginController.java
+++ src/main/java/web/LoginController.java
@@ -1,22 +1,16 @@
 package web;
 
-import entity.User;
+import com.google.code.kaptcha.Constants;
 import exception.CustomException;
 import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.authc.AuthenticationException;
 import org.apache.shiro.authc.UsernamePasswordToken;
-import org.apache.shiro.crypto.hash.Md5Hash;
 import org.apache.shiro.subject.Subject;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import service.ShiroUserService;
-import util.MD5Util;
-
 import javax.servlet.http.HttpServletRequest;
-import java.rmi.activation.UnknownObjectException;
 
 /**
  * Created by xuweijie on 2017/3/3.
@@ -27,24 +21,29 @@ public class LoginController {
     @RequestMapping(value = "/login",method = RequestMethod.POST)
     public String login(HttpServletRequest request, Model model){
         CustomException customException=null;
+        String verifyCode=request.getParameter("verifyCode").toUpperCase();
         String username=request.getParameter("username");
         String password=request.getParameter("password");
-        if((username!=null && password!=null)){
-            UsernamePasswordToken token=new UsernamePasswordToken(username,password);
-            Subject subject= SecurityUtils.getSubject();
-            try{
-                subject.login(token);
-            }catch (AuthenticationException e){
-                customException=new CustomException(e.getMessage());
+        if(verifyCode.equals(request.getSession().getAttribute(Constants.KAPTCHA_SESSION_KEY))){
+            if((username!=null && password!=null)){
+                UsernamePasswordToken token=new UsernamePasswordToken(username,password);
+                Subject subject= SecurityUtils.getSubject();
+                try{
+                    subject.login(token);
+                }catch (AuthenticationException e){
+                    customException=new CustomException(e.getMessage());
+                }
+                if( subject.isAuthenticated()){
+                    subject.logout();
+                    model.addAttribute("username",username);
+                    return "/loginsuccess";
+                }else {
+                    model.addAttribute("exception",customException.getMessage());
+                    return "/refuse";
+                }
             }
-            if( subject.isAuthenticated()){
-                subject.logout();
-                model.addAttribute("username",username);
-                return "/loginsuccess";
-            }else {
-                model.addAttribute("exception",customException.getMessage());
-                return "/refuse";
-            }
+        }else {
+            System.out.print("验证码输入不正确");
         }
         return "login";
     }
--- src/main/webapp/WEB-INF/jsp/login.jsp
+++ src/main/webapp/WEB-INF/jsp/login.jsp
@@ -2,6 +2,12 @@
 <html>
 <head>
     <title>用户登录</title>
+    <script type="text/javascript">
+        function changeVerifyCode() {
+            var time=new Date().getTime();
+            document.getElementById("kaptchaImage").src="/kaptcha?d="+time;//为了不让验证码缓存，为了安全起见，需要次次都刷新
+        }
+    </script>
 </head>
 
 <body>
@@ -10,8 +16,10 @@
         <h1>登录</h1>
         <br><br><br>
         <form action="/login" method="POST">
-            用&nbsp;&nbsp;户&nbsp;&nbsp;名：<input type="text" name="username"><br><br><br>
-            密&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;码：<input type="password" name="password"><br><br>
+            用&nbsp;&nbsp;户&nbsp;&nbsp;名：<input type="text" name="username" placeholder="请输入用户名"><br><br><br>
+            密&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;码：<input type="password" name="password" placeholder="请输入密码"><br><br>
+            验&nbsp;&nbsp;证&nbsp;&nbsp;码：<input type="text" name="verifyCode" placeholder="请输入验证码"/>
+            <img src="/kaptcha.jpg" id="kaptchaImage" title="看不清，点击换一张" onclick="changeVerifyCode()"><br><br>
             <input type="submit" value="登录">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<input type="reset" value="取消">
         </form>
     </div>
